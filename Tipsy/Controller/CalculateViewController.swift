7d9685e9a63cb10604b4140b5ee3d11d79d74fdf
Christina Labay <Christina.m.labay@gmail.com>
2020-09-02 11:01:13 -0500

    calculate total

--- Tipsy/Controller/CalculateViewController.swift
+++ Tipsy/Controller/CalculateViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class CalculateViewController: UIViewController {
     
-    var tipPercent: Float = 0.0
+    var tipPercent = 0.0
+    var numPeople = 2
 
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var billTextField: UITextField!
@@ -18,7 +19,7 @@ class CalculateViewController: UIViewController {
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumLabel: UILabel!
     @IBAction func tipChanged(_ sender: UIButton) {
-        
+        billTextField.resignFirstResponder()
         let btnPressed = sender.currentTitle
         if btnPressed == "0%" {
             tenPctButton.isSelected = false
@@ -38,10 +39,17 @@ class CalculateViewController: UIViewController {
         }
     }
     @IBAction func stepValueChanged(_ sender: UIStepper) {
+        billTextField.resignFirstResponder()
+        numPeople = Int(sender.value)
+        splitNumLabel.text = String(numPeople)
     }
     
     @IBAction func calcPressed(_ sender: UIButton) {
-        print( String(tipPercent))
+        if let total = billTextField.text {
+            let tip = Double(total)! * tipPercent
+            let split = ( Double(total)! + tip ) / Double(numPeople)
+            let resultTo2DecimalPlaces = String(format: "%.2f", split)
+        }
     }
 
 
